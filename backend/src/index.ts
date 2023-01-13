2e08895307e4ef5877c0f5d1b422a51b9d0165e9
Michael Stifter <75159397+neonalps@users.noreply.github.com>
2023-01-13 14:48:39 +0100

    Add basic Fastify server with hello endpoint (#2)
    Co-authored-by: Michael Stifter <michael.stifter@appradar.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log('🎵 Hello from on-repeat 🎵');
+import fastify from 'fastify';
+
+const server = fastify();
+
+server.get('/hello', async (request, reply) => {
+  return `🎵 Hello from on-repeat 🎵`;
+});
+
+server.listen({ port: 3003 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
