6837d928c1cb3a380d893ff632e4a0d4be934842
Michael Stifter <75159397+neonalps@users.noreply.github.com>
2023-01-13 15:20:57 +0100

    Add environment variable configuration (#3)
    Co-authored-by: Michael Stifter <michael.stifter@appradar.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,16 @@
-import fastify from 'fastify';
+import dotenv from "dotenv";
+import * as env from "env-var";
+import fastify from "fastify";
 
+dotenv.config();
+const PORT: number = env.get('PORT').required().asIntPositive();
 const server = fastify();
 
 server.get('/hello', async (request, reply) => {
   return `🎵 Hello from on-repeat 🎵`;
 });
 
-server.listen({ port: 3003 }, (err, address) => {
+server.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
