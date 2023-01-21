093719eb6a9e66c80310b42643db70210be6a443
Michael Stifter <75159397+neonalps@users.noreply.github.com>
2023-01-21 10:04:52 +0100

    Feature/add job runner skeleton (#7)
    * Add postgres dependencies and set up database migrations (#4)
    
    Co-authored-by: Michael Stifter <michael.stifter@appradar.com>
    
    * Init database structure (#5)
    
    Co-authored-by: Michael Stifter <michael.stifter@appradar.com>
    
    * Add first version of crypto util and adapt Typescript config (#6)
    
    Co-authored-by: Michael Stifter <michael.stifter@appradar.com>
    
    * Add job runner skeleton
    
    Co-authored-by: Michael Stifter <michael.stifter@appradar.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,18 @@
-import dotenv from "dotenv";
-import * as env from "env-var";
 import fastify from "fastify";
+import { getServerHost, getServerPort } from "@src/configuration";
+import { scheduler } from '@jobs/scheduler';
 
-dotenv.config();
-const PORT: number = env.get('PORT').required().asIntPositive();
 const server = fastify();
 
 server.get('/hello', async (request, reply) => {
   return `🎵 Hello from on-repeat 🎵`;
 });
 
-server.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
+server.listen({ host: getServerHost(), port: getServerPort() }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
   console.log(`Server listening at ${address}`);
+  scheduler.run();
 });
