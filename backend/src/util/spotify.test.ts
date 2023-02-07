ca528a082a0ba70d00e2961b56b3e77f182fcbfe
Michael Stifter <michael@neonalps.at>
2023-02-07 22:22:13 +0100

    Extend tests

--- backend/src/util/spotify.test.ts
+++ backend/src/util/spotify.test.ts
@@ -4,5 +4,6 @@ import { getAuthHeader } from './spotify';
 describe('SpotifyUtils', () => {
   test('returns correct Spotify auth header', () => {
     expect(getAuthHeader("client", "secret")).toBe("Y2xpZW50OnNlY3JldA==");
+    expect(getAuthHeader("", "")).toBe("Og==");
   });
 });
