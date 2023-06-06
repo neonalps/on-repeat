665a56f1afe77eb581b3524ad9c6f5abc15cb312
Michael Stifter <michael@neonalps.at>
2023-06-06 18:52:26 +0200

    Rename local provider to API

--- backend/src/migrations/20230511100915212_add-initial-music-providers.ts
+++ backend/src/migrations/20230511100915212_add-initial-music-providers.ts
@@ -5,7 +5,7 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
     pgm.sql(`insert into music_provider (name, display_name, created_at) values ('spotify', 'Spotify', now())`);
-    pgm.sql(`insert into music_provider (name, display_name, created_at) values ('local', 'Local', now())`);
+    pgm.sql(`insert into music_provider (name, display_name, created_at) values ('api', 'API', now())`);
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {}
