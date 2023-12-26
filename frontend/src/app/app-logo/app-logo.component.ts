e59824765c872f59fc07a1cb24513cb416d8bbdd
Michael Stifter <michael@neonalps.at>
2023-12-26 20:41:33 +0100

    feat: add app logo component

---
+++ frontend/src/app/app-logo/app-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-logo.component.html',
+  styleUrl: './app-logo.component.css'
+})
+export class AppLogoComponent {
+  color: string = "white"
+}
