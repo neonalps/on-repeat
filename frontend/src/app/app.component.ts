f649d6952afc55b0a6a14c03663f3fe3cc4ab48d
Michael Stifter <75159397+neonalps@users.noreply.github.com>
2023-12-27 22:16:09 +0100

    feat: add ngrx and use it for auth and ui state (#80)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { AppLogoComponent } from './app-logo/app-logo.component';
+import { AppLogoComponent } from '@src/app/app-logo/app-logo.component';
+import { Store } from '@ngrx/store';
+import { selectMenuVisible } from '@src/app/ui-state/store/ui-state.selectors';
+import { AppState } from '@src/app/store.index';
+import { toggleMenu } from '@src/app/ui-state/store/ui-state.actions';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppLogoComponent, CommonModule, RouterOutlet],
+  imports: [
+    AppLogoComponent, 
+    CommonModule, 
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'on-repeat-frontend';
+  menuVisible$ = this.store.select(selectMenuVisible);
+
+  constructor(private readonly store: Store<AppState>) {}
+
+  toggle() {
+    this.store.dispatch(toggleMenu());
+  }
+
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { authReducer } from './auth/store/auth.reducer';
+import { uiStateReducer } from './ui-state/store/ui-state.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes), 
+    provideStore({
+      auth: authReducer,
+      ui: uiStateReducer,
+    }),
+  ],
 };
