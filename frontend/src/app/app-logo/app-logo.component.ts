801166f7f42c1f718d1f559108d0baa6a53a20d0
Michael Stifter <michael@neonalps.at>
2024-01-06 15:34:40 +0100

    feat: improve logo alignment

--- frontend/src/app/app-logo/app-logo.component.ts
+++ frontend/src/app/app-logo/app-logo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-app-logo',
@@ -9,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppLogoComponent {
   color: string = "white"
+
+  @Input() top: number = 0;
+  @Input() width: number = 32;
 }
